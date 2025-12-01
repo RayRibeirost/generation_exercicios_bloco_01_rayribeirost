abf98fc9d8f87029949a67e8b04b774f2402fb04
Raylander Ribeiro (rayribeirost) <ribeiroraylander7@gmail.com>
2025-12-01 10:41:53 -0300

    adding day 06 (dez-01-2025) exercises file, 2 of 8 exercises completed and tested

---
+++ exercicios_dia_6_01_dez_2025.js
@@ -0,0 +1,51 @@
+"use strict";
+import readline from "readline-sync";
+
+//exercício 01
+
+/* let numA, numB, numC, soma, resultado
+numA = readline.questionInt("Digite o número A: ");
+numB = readline.questionInt("Digite o número B: ");
+numC = readline.questionInt("Digite o número C: ");
+soma = numA + numB;
+if (soma === numC) {
+    resultado = "Igual a";
+} else if (soma > numC) {
+    resultado = "Maior do que";
+} else {
+    resultado = "Menor do que"
+}
+console.log(`A Soma de A + B é ${resultado} C`); */
+
+
+//exercício 02
+
+let num;
+num = readline.questionInt("Digite um número: ");
+console.log(`O Número ${num} é ${num % 2 === 0 ? "par" : "ímpar"} e ${num > 0 ? "positivo" : "negativo"}!`);
+
+
+//exercício 03
+
+
+
+//exercício 04
+
+
+
+//exercício 05
+
+
+
+//exercício 06
+
+
+
+//exercício 07
+
+
+
+//exercício 08
+
+
+
