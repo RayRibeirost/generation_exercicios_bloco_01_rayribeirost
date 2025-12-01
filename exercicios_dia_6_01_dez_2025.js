3a5a3aa1f67fd9426872ea53fe034c0b4ab8be90
Raylander Ribeiro (rayribeirost) <ribeiroraylander7@gmail.com>
2025-12-01 12:02:09 -0300

    edit: day 06, exercices 5 and 6 completed and tested

--- exercicios_dia_6_01_dez_2025.js
+++ exercicios_dia_6_01_dez_2025.js
@@ -86,11 +86,89 @@ console.log(saida) */
 
 //exercício 05
 
+/* let codproduto, produto, preco, qtd, total;
 
+codproduto = readline.questionInt("Insira o código do produto (1 a 6), conforme o cardápio: ");
+qtd = readline.questionInt("Digite a quantidade desejada: ");
+
+switch (codproduto) {
+  case 1:
+    produto = "Cachorro quente";
+    preco = 10;
+    break;
+  case 2:
+    produto = "X-salada";
+    preco = 15;
+    break;
+  case 3:
+    produto = "X-bacon";
+    preco = 18;
+    break;
+  case 4:
+    produto = "Bauru";
+    preco = 12;
+    break;
+  case 5:
+    produto = "Refrigerante";
+    preco = 8;
+    break;
+  case 6:
+    produto = "Suco de laranja";
+    preco = 13;
+    break;
+}
+
+if (codproduto >= 1 && codproduto <= 6) {
+    total = qtd * preco
+    console.log(`Produto: ${produto}`);
+    console.log(`Valor Total: R$${total.toFixed(2)}`);
+} else {
+    console.log("Você digitou um código inválido, tente novamente.")   
+}   */
 
 //exercício 06
 
+/* let nomeColaborador, codCargo, cargo, percentual, salario, salarioReajustado;
 
+nomeColaborador = readline.question("Digite o nome do colaborador: ")
+codCargo = readline.questionInt("Insira o código do cargo (1 a 6): ");
+salario = readline.questionInt("Digite o salário atual: ");
+
+switch (codCargo) {
+  case 1:
+    cargo = "Gerente";
+    percentual = 0.1;
+    break;
+  case 2:
+    cargo = "Vendedor";
+    percentual = 0.07;
+    break;
+  case 3:
+    cargo = "Supervisor";
+    percentual = 0.09;
+    break;
+  case 4:
+    cargo = "Motorista";
+    percentual = 0.06;
+    break;
+  case 5:
+    cargo = "Estoquista";
+    percentual = 0.05;
+    break;
+  case 6:
+    cargo = "Técnico de TI";
+    percentual = 0.08;
+    break;
+}
+
+if (codCargo >= 1 && codCargo <= 6) {
+  salarioReajustado = salario + (percentual * salario);
+  console.log(`Nome do colaborador: ${nomeColaborador}`)
+  console.log(`Cargo: ${cargo}`);
+  console.log(`Salário: R$${salarioReajustado.toFixed(2)}`);
+} else {
+  console.log("Você digitou um código de cargo inválido, tente novamente.");
+}   */
 
 //exercício 07
 
