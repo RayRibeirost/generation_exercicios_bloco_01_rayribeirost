c637f549f7fa653b9422c5cf6c442218a0761df8
Raylander Ribeiro (rayribeirost) <ribeiroraylander7@gmail.com>
2025-12-01 11:30:39 -0300

    edit: day 06, exercices 3 and 4 completed and tested

--- exercicios_dia_6_01_dez_2025.js
+++ exercicios_dia_6_01_dez_2025.js
@@ -1,5 +1,5 @@
 "use strict";
-import readline from "readline-sync";
+import readline, { question } from "readline-sync";
 
 //exercício 01
 
@@ -20,18 +20,69 @@ console.log(`A Soma de A + B é ${resultado} C`); */
 
 //exercício 02
 
-let num;
+/* let num;
 num = readline.questionInt("Digite um número: ");
-console.log(`O Número ${num} é ${num % 2 === 0 ? "par" : "ímpar"} e ${num > 0 ? "positivo" : "negativo"}!`);
+console.log(`O Número ${num} é ${num % 2 === 0 ? "par" : "ímpar"} e ${num > 0 ? "positivo" : "negativo"}!`); */
 
 
 //exercício 03
 
+/* let nome, idade, ehPrimeiraDoacao, sexo, estaApto;
+
+nome = readline.question("Digite o nome do doador: ");
+idade = readline.questionInt("Digite a idade do doador: ");
+sexo = readline.keyIn("Qual seu sexo? (f/m) ", {limit: 'fmFM',});
+ehPrimeiraDoacao = readline.keyInYN("Primeira doação de sangue? ");
+
+if (idade > 18 && idade < 60) {
+    estaApto = true;
+} else if (idade >= 60 && idade <= 69 && ehPrimeiraDoacao === false) {
+    estaApto = true;
+} else {
+    estaApto = false;
+}
+
+console.log(`${nome} ${estaApto ? "está" : "não está"} ${sexo.toLowerCase() === "f" ? "apta" : "apto"} para doar sangue!`); */
 
 
 //exercício 04
 
+/* let entrada, saida;
 
+saida = "Desconhecido. Vc provavelmente digitou alguma das opções de forma errada.";
+
+const newQuestion = (questionSent) => {
+    const question = readline.question(questionSent).toLocaleLowerCase();
+    return question;
+}
+
+entrada = newQuestion("Digite se é vertebrado ou invertebrado: ");
+
+if (entrada === "vertebrado") {
+    entrada = newQuestion("Agora, digite se é ave ou mamifero: ");
+    if (entrada === "ave") {
+        entrada = newQuestion("Digite se é onivoro ou carnivoro: ");
+        if (entrada === "onivoro") saida = "Pomba";
+        if (entrada === "carnivoro") saida = "Águia";
+    } else if (entrada === "mamifero") {
+        entrada = newQuestion("Digite se é onivoro ou herbivoro: ");
+        if (entrada === "onivoro") saida = "Homem";
+        if (entrada === "herbivoro") saida = "Vaca";
+    }
+} else if (entrada === "invertebrado") {
+    entrada = newQuestion("Agora, digite se é inseto ou anelideo: ");
+    if (entrada === "inseto") {
+      entrada = newQuestion("Digite se é hematofago ou herbivoro: ");
+      if (entrada === "hematofago") saida = "Pulga";
+      if (entrada === "herbivoro") saida = "Lagarta";
+    } else if (entrada === "anelideo") {
+      entrada = newQuestion("Digite se é hematofago ou onivoro: ");
+      if (entrada === "onivoro") saida = "Sanguessuga";
+      if (entrada === "herbivoro") saida = "Minhoca";
+    }
+} 
+
+console.log(saida) */
 
 //exercício 05
 
