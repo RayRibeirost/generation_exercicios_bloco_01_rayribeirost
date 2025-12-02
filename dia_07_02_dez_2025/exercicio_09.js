1053b785d3d1bf08e0893cbc7e115783fa68acbb
Raylander Ribeiro (rayribeirost) <ribeiroraylander7@gmail.com>
2025-12-02 14:30:01 -0300

    update: exercises 09 and 10 of 10 completed and tested

--- dia_07_02_dez_2025/exercicio_09.js
+++ dia_07_02_dez_2025/exercicio_09.js
@@ -1,2 +1,31 @@
 "use strict";
 import readline from "readline-sync";
+
+let mainArr = [[], [], []], mainDiagArr = [], secDiagArr = []
+let mainDiagSum = 0, secDiagSum = 0;
+
+for(let i = 0; i < mainArr.length; i++) {
+    for(let j = 0; j < 3; j++) {
+        mainArr[i][j] = readline.questionInt(`Digite o ${j+1} número da linha ${i+1}: `);
+        if (i === j) {
+            mainDiagArr[mainDiagArr.length] = mainArr[i][j];
+            mainDiagSum += mainArr[i][j];
+        };
+        if (i + j === 2) {
+            secDiagArr[secDiagArr.length] = mainArr[i][j];
+            secDiagSum += mainArr[i][j];
+        }
+    }
+}
+
+console.log("Elementos da Diagonal Principal:");
+console.log(...mainDiagArr);
+
+console.log("Elementos da Diagonal Secundária:");
+console.log(...secDiagArr);
+
+console.log("Soma dos elementos da diagonal principal:")
+console.log(mainDiagSum);
+
+console.log("Soma dos elementos da diagonal secundária:")
+console.log(secDiagSum);
--- dia_07_02_dez_2025/exercicio_10.js
+++ dia_07_02_dez_2025/exercicio_10.js
@@ -1,2 +1,19 @@
 "use strict";
 import readline from "readline-sync";
+
+let gradesArr = [[], [], [], [], [], [], [], [], [], []], meanArr = []
+let gradesSum = 0
+
+for(let i = 0; i < 10; i++) {
+    for(let j = 0; j < 4; j++) {
+        gradesArr[i][j] = readline.questionFloat(`Digite a ${j+1} do aluno ${i+1}: `);
+        if (j === 3) {
+            for(const grade of gradesArr[i]) {
+                gradesSum += grade;
+            }
+            meanArr[meanArr.length] = (gradesSum / 4).toFixed(1);
+            gradesSum = 0;
+        }
+    }
+}
+console.log(...meanArr)
