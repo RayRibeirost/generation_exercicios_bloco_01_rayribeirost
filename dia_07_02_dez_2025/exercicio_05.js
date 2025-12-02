ae4a3397b8ab4ced6491f8f0c9db45f7c70b302a
Raylander Ribeiro (rayribeirost) <ribeiroraylander7@gmail.com>
2025-12-02 13:09:22 -0300

    update: exercises 05 and 06 of 10 completed and tested

--- dia_07_02_dez_2025/exercicio_05.js
+++ dia_07_02_dez_2025/exercicio_05.js
@@ -1,2 +1,12 @@
 "use strict";
 import readline from "readline-sync";
+
+let num, sum = 0;
+
+do {
+    num = readline.questionInt("Digite um número: ");
+    if (num > 0) sum += num;
+} while(num !== 0);
+
+console.log(`A soma dos números positivos é: ${sum}`);
+
--- dia_07_02_dez_2025/exercicio_06.js
+++ dia_07_02_dez_2025/exercicio_06.js
@@ -1,2 +1,14 @@
 "use strict";
 import readline from "readline-sync";
+
+let num, sum = 0, counter = 0;
+
+do {
+    num = readline.questionInt("Digite um número: ");
+    if (num % 3 === 0 && num !== 0) {
+        counter++;
+        sum += num;
+    }
+} while(num !== 0);
+
+console.log(`A média de todos os números múltiplos de 3 é: ${(sum / counter).toFixed(2)}`);
