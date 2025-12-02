1b6a57410e0f67053dcb916c98f6f5b48915d29b
Raylander Ribeiro (rayribeirost) <ribeiroraylander7@gmail.com>
2025-12-02 13:43:44 -0300

    update: exercises 07 and 08 of 10 completed and tested

--- dia_07_02_dez_2025/exercicio_07.js
+++ dia_07_02_dez_2025/exercicio_07.js
@@ -1,2 +1,16 @@
 "use strict";
 import readline from "readline-sync";
+
+let num, index;
+const arr = [2, 5, 1, 3, 4, 9, 7, 8, 10, 6];
+
+num = readline.questionInt("Digite o número que você deseja encontrar: ");
+
+for (let i = 0; i < arr.length; i++) {
+    if (num === arr[i]) {
+        index = i;
+        break;
+    };
+}
+
+console.log(index ? `O número ${num} está localizado na posição: ${index}` : `O número ${num} não foi encontrado!`);
--- dia_07_02_dez_2025/exercicio_08.js
+++ dia_07_02_dez_2025/exercicio_08.js
@@ -1,2 +1,25 @@
 "use strict";
 import readline from "readline-sync";
+
+let num, sum = 0, arr = [], oddIndexArr = [], evenElArr = []; 
+
+
+console.log("Primeiro, insira 10 números");
+
+for(let i = 0; i < 10; i++) {
+    num = readline.questionInt(`Digite o ${i + 1} número: `);
+    arr[i] = num;
+}
+
+for(let i = 0; i < 10; i++) {
+    if (i % 2 !== 0) oddIndexArr[oddIndexArr.length] = arr[i];
+    if (arr[i] % 2 === 0) evenElArr[evenElArr.length] = arr[i];
+    sum += arr[i]
+}
+
+console.log("Elementos nos índices ímpares: ");
+console.log(...oddIndexArr);
+console.log("Elementos pares:");
+console.log(...evenElArr)
+console.log(`Soma: ${sum}`);
+console.log(`Média: ${(sum / arr.length).toFixed(2)}`);
