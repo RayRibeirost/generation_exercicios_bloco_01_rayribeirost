eccce31f8d4e3c6abda7e9c9fe87e5cbf64236d9
Raylander Ribeiro (rayribeirost) <ribeiroraylander7@gmail.com>
2025-12-03 11:42:08 -0300

    update: day 08 exercises completed!

--- dia_08_03_dez_2025/exercicio_03.ts
+++ dia_08_03_dez_2025/exercicio_03.ts
@@ -1,1 +1,12 @@
 import readline from "readline-sync";
+
+let numSet: Set<number> = new Set<number>();
+let numInput: number;
+
+for(let i: number = 0; i < 10; i++) {
+    numInput = readline.questionInt(`Digite o ${i + 1} número: `);
+    numSet.add(numInput);
+}
+
+console.log("Listando dados do Set:");
+console.log(...numSet);
--- dia_08_03_dez_2025/exercicio_04.ts
+++ dia_08_03_dez_2025/exercicio_04.ts
@@ -1,2 +1,6 @@
 import readline from "readline-sync";
 
+const numSet: Set<number> = new Set<number>([2, 5, 1, 3, 4, 9, 7, 8, 10, 6]);
+let numInput: number = readline.questionInt("Digite o número que você deseja encontrar: ");
+let indexFounded: boolean = numSet.has(numInput);
+console.log(`O número ${numInput} ${indexFounded ? "foi encontrado!" : "não foi encontrado!"}`);
