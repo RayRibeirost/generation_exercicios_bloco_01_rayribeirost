dbefc001a5722b719c4d6783b008ccce397c8354
Raylander Ribeiro (rayribeirost) <ribeiroraylander7@gmail.com>
2025-12-04 11:16:02 -0300

    update: queue and stack documents added, day 09 exercises completed and tested

--- dia_09_04_dez_2025/exercicio_01.ts
+++ dia_09_04_dez_2025/exercicio_01.ts
@@ -1,1 +1,50 @@
 import readline from "readline-sync";
+import { Queue } from "../src/data-structures/Queue.js";
+
+const customerQueue: Queue<string> = new Queue<string>;
+let option: number, customerName: string, exit: boolean = false;
+
+do {
+    console.log("************************************************");
+    console.log("1 - Adicionar Cliente na Fila");
+    console.log("2 - Listar todos os Clientes");
+    console.log("3 - Retirar cliente da fila");
+    console.log("0 - Sair");
+    console.log("************************************************");
+    option = readline.questionInt("Entre com a opção desejada: ");
+
+    switch(option) {
+        case 1:
+            customerName = readline.question("Digite o nome: ");
+            customerQueue.enqueue(customerName);
+            console.log("Fila:");
+            customerQueue.printQueue();
+            console.log("Cliente Adicionado!");
+            break;
+        case 2:
+            if (customerQueue.isEmpty()) {
+                console.log("A fila está vazia!");
+            } else {
+                console.log("Lista de Clientes na Fila:");
+                customerQueue.printQueue();
+            };
+            break;
+        case 3:
+            if (customerQueue.isEmpty()) {
+                console.log("A fila está vazia!");
+            } else {
+                customerQueue.dequeue();
+                console.log("Fila:");
+                customerQueue.printQueue();
+                console.log("O cliente foi chamado!");
+            };
+            break;
+        case 0:
+            exit = true;
+            break;
+        default:
+            console.log("Insira uma opção válida!") 
+    }
+} while(!exit);
+
+console.log("Programa finalizado!")
--- dia_09_04_dez_2025/exercicio_02.ts
+++ dia_09_04_dez_2025/exercicio_02.ts
@@ -1,1 +1,50 @@
 import readline from "readline-sync";
+import { Stack } from "../src/data-structures/Stack.js";
+
+const bookStack: Stack<string> = new Stack<string>;
+let option: number, bookName: string, exit: boolean = false;
+
+do {
+    console.log("************************************************");
+    console.log("1 - Adicionar Livro na Pilha");
+    console.log("2 - Listar todos os Livros");
+    console.log("3 - Retirar Livro da Pilha");
+    console.log("0 - Sair");
+    console.log("************************************************");
+    option = readline.questionInt("Entre com a opção desejada: ");
+
+    switch(option) {
+        case 1:
+            bookName = readline.question("Digite o nome: ");
+            bookStack.push(bookName);
+            console.log("Pilha:");
+            bookStack.printStack();
+            console.log("Livro Adicionado!");
+            break;
+        case 2:
+            if (bookStack.isEmpty()) {
+                console.log("A pilha está vazia!");
+            } else {
+                console.log("Lista de livros na pilha:");
+                bookStack.printStack();
+            };
+            break;
+        case 3:
+            if (bookStack.isEmpty()) {
+                console.log("A pilha está vazia!");
+            } else {
+                bookStack.pop();
+                console.log("Pilha:");
+                bookStack.printStack();
+                console.log("O livro foi retirado da pilha!");
+            };
+            break;
+        case 0:
+            exit = true;
+            break;
+        default:
+            console.log("Insira uma opção válida!") 
+    }
+} while(!exit);
+
+console.log("Programa finalizado!")
